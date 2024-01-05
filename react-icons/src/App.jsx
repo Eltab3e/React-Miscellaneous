8bda2caa43fa4dc91b12d6fe4dfaa99c032db7eb
Eltab3e <ahmed.eltabiie@gmail.com>
2024-01-05 17:02:46 +0200

    react icons example

--- react-icons/src/App.jsx
+++ react-icons/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { IconContext } from "react-icons/lib";
+import { FaHtml5, FaCss3, FaReact } from "react-icons/fa";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <IconContext.Provider value={{ size: "10rem" }}>
+            <div>
+                <FaHtml5 color="orange" />
+                <FaCss3 color="blue" />
+                <FaReact color="purple" />
+            </div>
+        </IconContext.Provider>
+    );
 }
 
-export default App
+export default App;
