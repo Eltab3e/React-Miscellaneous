e42a1cb4ca61dda85c86114128506b81f227667f
Eltab3e <ahmed.eltabiie@gmail.com>
2024-01-09 15:39:15 +0200

    react video player example

---
+++ react-video-player/src/App.jsx
@@ -0,0 +1,15 @@
+import ReactPlayer from "react-player";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <ReactPlayer
+                controls
+                url="https://www.youtube.com/watch?v=34Na4j8AVgA"
+            />
+        </div>
+    );
+}
+
+export default App;
